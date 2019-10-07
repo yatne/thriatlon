48a461739744555cb1d7ca0c989f81f6910a9a03
Antoni <antonimaciej@gmail.com>
2019-10-07 13:57:56 +0200

    added simple workout model

--- backend/thriatlon/workouts/models.py
+++ backend/thriatlon/workouts/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Workout(models.Model):
+    user = models.CharField(max_length=50)
+    discipline = models.CharField(max_length=50)
+    distance = models.FloatField()
--- backend/thriatlon/workouts/views.py
+++ backend/thriatlon/workouts/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Workout
+from .serializers import WorkoutSerializer
 
-# Create your views here.
+class WorkoutView(viewsets.ModelViewSet):
+    queryset = Workout.objects.all()
+    serializer_class = WorkoutSerializer
